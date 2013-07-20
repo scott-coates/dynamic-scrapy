152981ee97500eb12c1859ed7b8d0ea3aa17eaa1
Scott Coates <scoarescoare@gmail.com>
2013-07-20 10:48:33 -0700

    added temporary monkey-patch to ensure absolute url is created

--- scrapy_test/apps/web_scraper/spiders/listing_spider.py
+++ scrapy_test/apps/web_scraper/spiders/listing_spider.py
@@ -1,7 +1,23 @@
 from dynamic_scraper.spiders.django_spider import DjangoSpider
+from dynamic_scraper.utils import processors
 from scrapy_test.aggregates.listing.models import Listing
 from scrapy_test.aggregates.listing_source.models import ListingSource
 from scrapy_test.apps.web_scraper.model_proxies import ListingItem
+
+
+def pre_url_from_ref_object(text, loader_context):
+  ret_val = None
+
+  from urlparse import urljoin
+
+  base_url = loader_context['spider'].ref_object.url
+
+  ret_val = urljoin(base_url, text)
+
+  return ret_val
+
+
+setattr(processors, 'pre_url_from_ref_object', pre_url_from_ref_object)
 
 
 class ListingSpider(DjangoSpider):
