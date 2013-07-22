9250e25f40081a3f60b339da8f70ce4196d74504
Scott Coates <scoarescoare@gmail.com>
2013-07-21 20:51:22 -0400

    added CELERY_EAGER_PROPAGATES_EXCEPTIONS = True in dev.py

--- scrapy_test/settings/dev.py
+++ scrapy_test/settings/dev.py
@@ -50,6 +50,8 @@ CACHES = {
 # See: http://docs.celeryq.org/en/latest/configuration.html#celery-always-eager
 CELERY_ALWAYS_EAGER = True
 
+CELERY_EAGER_PROPAGATES_EXCEPTIONS = True
+
 BROKER_TRANSPORT = 'django'
 
 CELERY_RESULT_BACKEND = 'database'
