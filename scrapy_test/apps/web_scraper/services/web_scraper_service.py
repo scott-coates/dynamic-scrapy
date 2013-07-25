14e3207154712e16c729ed884255f81000069875
Scott Coates <scoarescoare@gmail.com>
2013-07-25 00:30:46 -0400

    using listing source config in listing_spider

--- scrapy_test/apps/web_scraper/services/web_scraper_service.py
+++ scrapy_test/apps/web_scraper/services/web_scraper_service.py
@@ -1,7 +1,7 @@
-from scrapy_test.aggregates.listing_source.models import ListingSource
+from scrapy_test.apps.web_scraper.models import ListingSourceScraperConfig
 from scrapy_test.apps.web_scraper.scrapy_extensions.utils_extensions import ProcessBasedUtils
 
 
 def run_spiders():
   t = ProcessBasedUtils()
-  t.run_spiders(ListingSource, 'scraper', 'scraper_runtime', 'listing_spider')
+  t.run_spiders(ListingSourceScraperConfig, 'scraper', 'scraper_runtime', 'listing_spider')
