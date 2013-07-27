9731960c25bc2149f758efe44e132f9c1d5d1db8
Scott Coates <scoarescoare@gmail.com>
2013-07-26 20:25:07 -0400

    now using dynamic processor func

--- scrapy_test/apps/web_scraper/scrapy_extensions/processor_extensions.py
+++ scrapy_test/apps/web_scraper/scrapy_extensions/processor_extensions.py
@@ -1,14 +1,9 @@
-from dynamic_scraper.utils import processors
+from urlparse import urljoin
 
-#todo put this in a fork and pull request
+
 def pre_url_from_ref_object(text, loader_context):
-  from urlparse import urljoin
-
   base_url = loader_context['spider'].ref_object.listing_source.url
 
   ret_val = urljoin(base_url, text)
 
   return ret_val
-
-
-setattr(processors, 'pre_url_from_ref_object', pre_url_from_ref_object)
