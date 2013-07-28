ae1ca7f928c482b73fdc4867d4f2d34817b6e0e0
Scott Coates <scoarescoare@gmail.com>
2013-07-28 14:24:16 -0400

    comment about invocation

--- scrapy_test/apps/web_scraper/scrapy_extensions/processor_extensions.py
+++ scrapy_test/apps/web_scraper/scrapy_extensions/processor_extensions.py
@@ -1,6 +1,7 @@
 from urlparse import urljoin
 
 
+#this is called from the admin. The listing spider defins this method as a processor and is invoked dynamically
 def pre_url_from_ref_object(text, loader_context):
   base_url = loader_context['spider'].ref_object.listing_source.url
 
