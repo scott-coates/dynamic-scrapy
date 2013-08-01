e876aa24c2cb8ef7e373e2d3c7df1eed31562180
Scott Coates <scoarescoare@gmail.com>
2013-07-31 22:45:29 -0400

    started to flesh out builder

--- scrapy_test/aggregates/listing/domain/listing_builder.py
+++ scrapy_test/aggregates/listing/domain/listing_builder.py
@@ -1,7 +1,18 @@
+bedroom_count = 'bedroom_count'
+
+
 class ListingBuilder(object):
-  def __init__(self):
+  def __init__(self, **kwargs):
+    self.listing_attrs_input = kwargs
+    self.listing_attrs_output = kwargs
     pass
 
-  def build_listing(self,**kwargs):
-    #figure out bed/bath
+  def _build_bedroom_count(self):
+    bed_count = self.listing_attrs_input.get(bedroom_count)
+    if not bed_count:
+      self.listing_attrs_output[bedroom_count] = 1
+
+  def build_listing(self):
+    self._build_bedroom_count()
+
     return 'foo'
