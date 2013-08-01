fa902953409e9d844affd7fec620875dfdc98ac4
Scott Coates <scoarescoare@gmail.com>
2013-07-31 21:30:46 -0400

    rename to pk

--- scrapy_test/aggregates/listing_source/services/listing_source_service.py
+++ scrapy_test/aggregates/listing_source/services/listing_source_service.py
@@ -1,5 +1,5 @@
 from scrapy_test.aggregates.listing_source.models import ListingSource
 
 
-def get_listing_source(id):
-  return ListingSource.objects.get(pk=id)
+def get_listing_source(pk):
+  return ListingSource.objects.get(pk=pk)
