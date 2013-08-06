881aba30a37844a514fb06f08fe3ae8cb9c26a43
Scott Coates <scoarescoare@gmail.com>
2013-08-06 09:20:44 -0400

    fixed property naming

--- scrapy_test/libs/common_domain/aggregate_base.py
+++ scrapy_test/libs/common_domain/aggregate_base.py
@@ -14,12 +14,12 @@ class AggregateBase(object):
     # get the fq name - this is used for replaying events
     event_fq_name = event.module_name + "." + event.name
 
-    self._uncommitted_events.append(EventRecord(event, event_name, version, event_fq_name, sender, kwargs))
+    self._uncommitted_events.append(EventRecord(event, event_name, event_fq_name, version, sender, kwargs))
 
   def send_events(self):
     while self._uncommitted_events:
-      event_def = self._uncommitted_events.popleft()
-      event_def[0].send(self, event_def[1], **event_def[2])
+      event_record = self._uncommitted_events.popleft()
+      event_record.event_obj.send(event_record.sender, **event_record.kwargs)
 
   def _apply_event(self, event, sender, **kwargs):
     # the last element in the providing_args is used to re-created the name of the event later
@@ -27,7 +27,7 @@ class AggregateBase(object):
 
     handle_func = getattr(self, event_func_name, None)
 
-    if not handle_func: raise NotImplementedError("{0} must implement {1}".format(self.__class__.__name__,
-                                                                                      event_func_name))
+    if not handle_func: raise NotImplementedError("{0} must implement {1}".format(
+      self.__class__.__name__, event_func_name))
 
     handle_func(**kwargs)
