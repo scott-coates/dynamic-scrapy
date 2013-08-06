3f52b13a54b5744481dd2f7892df8981fb10628c
Scott Coates <scoarescoare@gmail.com>
2013-08-05 21:39:04 -0400

    fixed name w/ unsan

--- scrapy_test/aggregates/listing/signals.py
+++ scrapy_test/aggregates/listing/signals.py
@@ -2,5 +2,5 @@ from scrapy_test.libs.common_domain.event_signal import EventSignal
 
 created = EventSignal('created', __name__, 1, providing_args=['instance', 'attrs'])
 sanitized = EventSignal('sanitized', __name__, 1, providing_args=['instance'])
-unsanitized = EventSignal('unsanitzed', __name__, 1, providing_args=['instance', 'errors'])
+unsanitized = EventSignal('unsanitized', __name__, 1, providing_args=['instance', 'errors'])
 deleted = EventSignal('deleted', __name__, 1, providing_args=['instance'])
