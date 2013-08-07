e9a68a83c5ed8873c30eb0e9b2fa4a0872c5b82c
Scott Coates <scoarescoare@gmail.com>
2013-08-06 23:42:05 -0400

    fixed issue with pk name

--- scrapy_test/libs/django_utils/models/utils.py
+++ scrapy_test/libs/django_utils/models/utils.py
@@ -4,7 +4,7 @@ def copy_django_model_attrs(instance_to_copy_to, **kwargs):
   new_kwargs = dict(
     [
       (fld.name, getattr(tmp_listing, fld.name)) for fld in tmp_listing._meta.fields if
-      fld.name != tmp_listing._meta.pk
+      fld.name != tmp_listing._meta.pk.name
     ]
   )
   for k, v in new_kwargs.items():
--- scrapy_test/libs/django_utils/tests/unit/utils.py
+++ scrapy_test/libs/django_utils/tests/unit/utils.py
@@ -16,3 +16,12 @@ class UtilsTestCase(SimpleTestCase):
     copy_django_model_attrs(self.test_model, **attrs)
     self.assertEqual(title, self.test_model.title)
 
+  def test_copy_django_model_attrs_copies_correct_attribute_only_if_called(self):
+    self.assertFalse(self.test_model.title)
+
+  def test_copy_django_model_attrs_does_not_copy_pk(self):
+    id_field=1
+    attrs = {'pk': id_field}
+    copy_django_model_attrs(self.test_model, **attrs)
+    self.assertNotEqual(id_field, self.test_model.pk)
+
