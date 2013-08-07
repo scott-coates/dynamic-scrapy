26e65e531a5f44f4d82f0ef9f31026319dda8d67
Scott Coates <scoarescoare@gmail.com>
2013-08-06 23:44:59 -0400

    added test for id or pk

--- scrapy_test/libs/django_utils/tests/unit/utils.py
+++ scrapy_test/libs/django_utils/tests/unit/utils.py
@@ -20,8 +20,8 @@ class UtilsTestCase(SimpleTestCase):
     self.assertFalse(self.test_model.title)
 
   def test_copy_django_model_attrs_does_not_copy_pk(self):
-    id_field=1
-    attrs = {'pk': id_field}
+    id_field = 1
+    attrs = {'pk': id_field, 'id': id_field}
     copy_django_model_attrs(self.test_model, **attrs)
     self.assertNotEqual(id_field, self.test_model.pk)
 
