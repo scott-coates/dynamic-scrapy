873186d274df98ec7ea6d9850c3caf292e11a9b9
Scott Coates <scoarescoare@gmail.com>
2013-08-06 23:18:53 -0400

    run test to ensure attrs are copied over

--- scrapy_test/libs/django_utils/tests/unit/utils.py
+++ scrapy_test/libs/django_utils/tests/unit/utils.py
@@ -1,25 +1,18 @@
-# # django model constructor has pretty smart logic for mass assignment
-# def copy_django_model_attrs(instance_to_copy_to, **kwargs):
-#   tmp_listing = instance_to_copy_to.__class__(**kwargs)
-#   new_kwargs = dict(
-#     [
-#       (fld.name, getattr(tmp_listing, fld.name)) for fld in tmp_listing._meta.fields if
-#       fld.name != tmp_listing._meta.pk
-#     ]
-#   )
-#   for k, v in new_kwargs.items():
-#     setattr(instance_to_copy_to, k, v)
-
 from django.test import SimpleTestCase
 from django.db import models
+from scrapy_test.libs.django_utils.models.utils import copy_django_model_attrs
 
 
 class UtilsTestCase(SimpleTestCase):
-  # class TestModel(models.Model):
-  #   title = models.TextField()
-  #
-  # def setUp(self):
-  #   self.test_model = UtilsTestCase.TestModel()
+  class TestModel(models.Model):
+    title = models.TextField()
+
+  def setUp(self):
+    self.test_model = UtilsTestCase.TestModel()
 
   def test_copy_django_model_attrs_copies_correct_attribute(self):
-    self.assertEqual(1, 1)
+    title = 'hi'
+    attrs = {'title': title}
+    copy_django_model_attrs(self.test_model, **attrs)
+    self.assertEqual(title, self.test_model.title)
+
