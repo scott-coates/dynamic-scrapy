f3d0520bf257585f6aad18fe5172c444bd54b2d0
Scott Coates <scoarescoare@gmail.com>
2013-08-08 13:04:40 -0400

    test to assert names are called correctly

--- scrapy_test/libs/common_domain/tests/unit/aggregate_base_tests.py
+++ scrapy_test/libs/common_domain/tests/unit/aggregate_base_tests.py
@@ -1,6 +1,6 @@
 from functools import partial
 from django.test import SimpleTestCase
-from mock import MagicMock
+from mock import MagicMock, create_autospec, ANY
 from scrapy_test.libs.common_domain.aggregate_base import AggregateBase
 
 
@@ -17,9 +17,9 @@ class AggregateBaseTestCase(SimpleTestCase):
     aggregate_test = AggregateBaseTestCase.TestAggregate()
 
     signal1 = MagicMock()
-    signal1.event_obj.send = MagicMock(side_effect=partial(side_effect,1))
+    signal1.event_obj.send = MagicMock(side_effect=partial(side_effect, 1))
     signal2 = MagicMock()
-    signal2.event_obj.send = MagicMock(side_effect=partial(side_effect,2))
+    signal2.event_obj.send = MagicMock(side_effect=partial(side_effect, 2))
 
     aggregate_test._uncommitted_events.append(signal1)
     aggregate_test._uncommitted_events.append(signal2)
@@ -27,3 +27,15 @@ class AggregateBaseTestCase(SimpleTestCase):
     aggregate_test.send_events()
 
     self.assertListEqual(results, [1, 2])
+
+  def test_aggregate_uses_correct_naming_convention_when_applying(self):
+    aggregate_test = AggregateBaseTestCase.TestAggregate()
+
+    event = MagicMock()
+    event.name = 'test'
+
+    aggregate_test._handle_test_event = MagicMock()
+
+    aggregate_test._apply_event(event)
+
+    aggregate_test._handle_test_event.assert_called_with()
