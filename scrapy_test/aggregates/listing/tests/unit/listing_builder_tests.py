555716fd0bd80702628de71cffa08fd8d82f3f69
Scott Coates <scoarescoare@gmail.com>
2013-08-09 15:44:14 -0400

    added test for error

--- scrapy_test/aggregates/listing/tests/unit/listing_builder_tests.py
+++ scrapy_test/aggregates/listing/tests/unit/listing_builder_tests.py
@@ -2,16 +2,26 @@ import pytest
 from scrapy_test.aggregates.listing.domain.listing_builder import ListingBuilder
 from scrapy_test.aggregates.listing.tests.unit import listing_test_data
 
-# region title
-title_trimmed_at_beginning = listing_test_data.cl_listing_3952467416['title']
-expected_title_trimmed_at_beginning = listing_test_data.cl_listing_3952467416_expected_title
-# endregion
+# region title tests
+title_stripped = listing_test_data.cl_listing_3952467416['title']
+expected_title_stripped = listing_test_data.cl_listing_3952467416_expected_title
+
+title_scalar = 'My Title'
+expected_title_scalar = 'My Title'
 
 
 @pytest.mark.parametrize(("input_values", "expected"), [
-  (title_trimmed_at_beginning, expected_title_trimmed_at_beginning),
+  (title_stripped, expected_title_stripped),
+  (title_scalar, expected_title_scalar),
 ])
 def test_builder_title(input_values, expected):
   builder = ListingBuilder(title=input_values)
   builder._build_summary()
   assert builder.listing_attrs_output['title'] == expected
+
+
+def test_builder_throws_appropriate_error_for_invalid_type():
+  with pytest.raises(TypeError):
+    builder = ListingBuilder(title=1)
+    builder._build_summary()
+# endregion
