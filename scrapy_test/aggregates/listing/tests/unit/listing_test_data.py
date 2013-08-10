d9e641c10bbf78c21c6ad486ba03ec3d7b119558
Scott Coates <scoarescoare@gmail.com>
2013-08-10 18:46:52 -0400

    added new cl data

--- scrapy_test/aggregates/listing/tests/unit/listing_test_data.py
+++ scrapy_test/aggregates/listing/tests/unit/listing_test_data.py
@@ -36,3 +36,48 @@ cl_listing_3952467416_expected_description = u'HOUSE FOR RENT IN GOOD LOCATION\n
 cl_listing_3952467416_expected_posted_date = eastern_time_zone.localize(datetime.datetime(2013, 8, 7, 16, 52))
 cl_listing_3952467416_expected_last_updated_date = eastern_time_zone.localize(datetime.datetime(2013, 7, 22, 15, 31))
 #endregion
+
+
+# region cl_listing_3970405942
+cl_listing_3970405942 = {u'city': [u'Bronx'], u'contact_phone_number': [
+  u'\n\t\t3 Bedroom Apartment with 2 Full Bath (full bath in Master Bedroom)',
+  u'\nSpacious Living Room & Dining Room. Brand New Granite Kitchen with Stainless Steel Appliances. Hardwood Floors '
+  u'All Throughout Apartment',
+  u'\n------Section 8 OK ', u'\n------Available 9/1/13', u'\nWALK DISTANCE TO #2 & #5 TRAINS ',
+  u'\n3 Brand New Gas Boilers & Hot Water Tanks. Massive Backyard. Near Shopping & Schools. ',
+  u'\nCALL 917-417-7648 (Se Habla Espanol) ', u'Listed By: Jose Rodriguez, Inc'],
+                         u'description': [u'\n\t\t3 Bedroom Apartment with 2 Full Bath (full bath in Master Bedroom)',
+                                          u'\nSpacious Living Room & Dining Room. Brand New Granite Kitchen with '
+                                          u'Stainless Steel Appliances. Hardwood Floors All Throughout Apartment',
+                                          u'\n------Section 8 OK ', u'\n------Available 9/1/13',
+                                          u'\nWALK DISTANCE TO #2 & #5 TRAINS ',
+                                          u'\n3 Brand New Gas Boilers & Hot Water Tanks. Massive Backyard. Near '
+                                          u'Shopping & Schools. ',
+                                          u'\nCALL 917-417-7648 (Se Habla Espanol) '],
+                         u'title': [u'\n  - $1850 / 3br - 3 Bedroom 2 Full Bath Luxury Apartment (Bronx, NY)\n'],
+                         u'url': u'http://newyork.craigslist.org/brx/abo/3970405942.html',
+                         u'address1': [u'Bristow St', u'Union'],
+                         u'price': [u'\n  - $1850 / 3br - 3 Bedroom 2 Full Bath Luxury Apartment (Bronx, NY)\n'],
+                         u'state': [u'NY'], u'last_updated_date': [u'2013-08-10,  6:19PM EDT'],
+                         u'posted_date': [u'2013-07-30,  7:38PM EDT', u'2013-07-30,  7:38PM EDT'],
+                         u'contact_email': [u'\n\t\t3 Bedroom Apartment with 2 Full Bath (full bath in Master Bedroom)',
+                                            u'\nSpacious Living Room & Dining Room. Brand New Granite Kitchen with '
+                                            u'Stainless Steel Appliances. Hardwood Floors All Throughout Apartment',
+                                            u'\n------Section 8 OK ', u'\n------Available 9/1/13',
+                                            u'\nWALK DISTANCE TO #2 & #5 TRAINS ',
+                                            u'\n3 Brand New Gas Boilers & Hot Water Tanks. Massive Backyard. Near '
+                                            u'Shopping & Schools. ',
+                                            u'\nCALL 917-417-7648 (Se Habla Espanol) ',
+                                            u'Listed By: Jose Rodriguez, Inc'],
+                         u'contact_name': [u'\n\t\t3 Bedroom Apartment with 2 Full Bath (full bath in Master Bedroom)',
+                                           u'\nSpacious Living Room & Dining Room. Brand New Granite Kitchen with Stainless Steel Appliances. Hardwood Floors All Throughout Apartment',
+                                           u'\n------Section 8 OK ', u'\n------Available 9/1/13',
+                                           u'\nWALK DISTANCE TO #2 & #5 TRAINS ',
+                                           u'\n3 Brand New Gas Boilers & Hot Water Tanks. Massive Backyard. Near Shopping & Schools. ',
+                                           u'\nCALL 917-417-7648 (Se Habla Espanol) ',
+                                           u'Listed By: Jose Rodriguez, Inc'], 'listing_source_id': 1, u'lat': [],
+                         u'bedroom_count': [
+                           u'\n  - $1850 / 3br - 3 Bedroom 2 Full Bath Luxury Apartment (Bronx, NY)\n'], u'lng': [],
+                         u'broker_fee': [u'/brx/abo/3970405942.html'],
+                         u'sqfeet': [u'\n  - $1850 / 3br - 3 Bedroom 2 Full Bath Luxury Apartment (Bronx, NY)\n']}
+#endregion
