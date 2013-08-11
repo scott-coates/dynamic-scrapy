0e0dfcaa699c67ecb62afcd31ccbd8249d00d66a
Scott Coates <scoarescoare@gmail.com>
2013-08-11 19:27:29 -0400

    added studio test

--- scrapy_test/libs/housing_utils/parsing/home_parser.py
+++ scrapy_test/libs/housing_utils/parsing/home_parser.py
@@ -1,5 +1,8 @@
 import re
 address2_pattern = re.compile(r"((\#|apt|suite|ste)\.?\s?\d+)")
 
-def get_bedroom_count(bedroom):
-  return 2
+def get_bedroom_count(bedroom_str):
+  ret_val = None
+  if 'studio' in bedroom_str:
+    ret_val = 0
+  return ret_val
