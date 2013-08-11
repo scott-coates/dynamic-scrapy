c0259cdf17f439c7a59f86fa31960c2797767b1a
Scott Coates <scoarescoare@gmail.com>
2013-08-11 17:43:02 -0400

    updated parser

--- scrapy_test/libs/housing_utils/parsing/home_parser.py
+++ scrapy_test/libs/housing_utils/parsing/home_parser.py
@@ -1,20 +1,5 @@
 import re
 address2_pattern = re.compile(r"((\#|apt|suite|ste)\.?\s?\d+)")
 
-def is_street_address(address):
-  address_split = [address_part for address_part in address.split() if address_part not in ("and", "at")]
-  return len(address_split) >= 3
-
-
-def is_cross_street_address(address):
-  return " and " in address or " at " in address
-
-def get_address2(address):
-  ret_val = None
-
-  match = address2_pattern.search(address)
-
-  if match:
-    ret_val = match.group()
-
-  return ret_val
+def get_bedroom_count(bedroom):
+  return 2
