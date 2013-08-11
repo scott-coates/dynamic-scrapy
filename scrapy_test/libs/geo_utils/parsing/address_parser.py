f71514c5039f4e386450faf6a7109cdbbecafc02
Scott Coates <scoarescoare@gmail.com>
2013-08-11 10:21:02 -0400

    added tests for addresses

--- scrapy_test/libs/geo_utils/parsing/address_parser.py
+++ scrapy_test/libs/geo_utils/parsing/address_parser.py
@@ -1,5 +1,7 @@
 def is_street_address(address):
-  return None
+  address_split = [address_part for address_part in address.split() if address_part not in ("and", "at")]
+  return len(address_split) >= 3
+
 
 def is_cross_street_address(address):
-  return None
+  return " and " in address or " at " in address
--- scrapy_test/libs/geo_utils/tests/unit/address_parser_tests.py
+++ scrapy_test/libs/geo_utils/tests/unit/address_parser_tests.py
@@ -4,7 +4,18 @@ from scrapy_test.libs.geo_utils.parsing import address_parser
 
 @pytest.mark.parametrize(("input_values", "expected"), [
   ('123 fake st', True),
+  ('100 e 55th', True),
   ('5th', False),
+  ('5th and 55th', False),
 ])
 def test_address_parser_detects_correct_street_addresses(input_values, expected):
   assert expected == address_parser.is_street_address(input_values)
+
+@pytest.mark.parametrize(("input_values", "expected"), [
+  ('62nd at york', True),
+  ('21 and broadway', True),
+  ('67 w 58th', False),
+  ('250 e66', False),
+])
+def test_address_parser_detects_correct_cross_street_addresses(input_values, expected):
+  assert expected == address_parser.is_cross_street_address(input_values)
