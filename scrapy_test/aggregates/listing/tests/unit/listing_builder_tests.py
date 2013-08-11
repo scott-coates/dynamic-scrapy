7989ae7dc7dfca448d9a0792b113e86c857e0398
Scott Coates <scoarescoare@gmail.com>
2013-08-11 17:30:21 -0400

    added housing utils

--- scrapy_test/aggregates/listing/tests/unit/listing_builder_tests.py
+++ scrapy_test/aggregates/listing/tests/unit/listing_builder_tests.py
@@ -234,5 +234,17 @@ def test_builder_gets_correct_bedroom_from_list():
 
   assert bedroom_count_attr == expected_bedroom_count
 
+def test_builder_gets_correct_bedroom_from_title_if_not_in_list():
+  bedroom_count = '2'
+
+  builder = ListingBuilder(bedroom_count=[bedroom_count])
+
+  builder._build_bedroom_count()
+
+  bedroom_count_attr = builder.listing_attrs_output[listing_builder.BEDROOM_COUNT]
+
+  expected_bedroom_count = 2
+
+  assert bedroom_count_attr == expected_bedroom_count
 
 # endregion
---
+++ scrapy_test/libs/housing_utils/tests/unit/home_parser_tests.py
@@ -0,0 +1,31 @@
+import pytest
+from scrapy_test.libs.geo_utils.parsing import address_parser
+
+
+@pytest.mark.parametrize(("input_values", "expected"), [
+  ('123 fake st', True),
+  ('100 e 55th', True),
+  ('5th', False),
+  ('5th and 55th', False),
+])
+def test_address_parser_detects_correct_street_addresses(input_values, expected):
+  assert expected == address_parser.is_street_address(input_values)
+
+@pytest.mark.parametrize(("input_values", "expected"), [
+  ('62nd at york', True),
+  ('21 and broadway', True),
+  ('Macon st at Marcy ave', True),
+  ('67 w 58th', False),
+  ('250 e66', False),
+])
+def test_address_parser_detects_correct_cross_street_addresses(input_values, expected):
+  assert expected == address_parser.is_cross_street_address(input_values)
+
+@pytest.mark.parametrize(("input_values", "expected"), [
+  ('123 fake st #99', '#99'),
+  ('123 fake st', None),
+  ('Some place ste 67', 'ste 67'),
+  ('Some place apt. 55', 'apt. 55'),
+])
+def test_address_parser_detects_address2(input_values, expected):
+  assert expected == address_parser.get_address2(input_values)
