a8be0e229c2eba065fa1c1f0ad093075a25373dc
Scott Coates <scoarescoare@gmail.com>
2013-08-12 11:42:26 -0400

    removed pytest

--- scrapy_test/libs/django_utils/tests/unit/utils_tests.py
+++ scrapy_test/libs/django_utils/tests/unit/utils_tests.py
@@ -1,27 +1,32 @@
-from django.test import SimpleTestCase
 from django.db import models
+import pytest
 from scrapy_test.libs.django_utils.models.utils import copy_django_model_attrs
 
 
-class UtilsTestCase(SimpleTestCase):
-  class TestModel(models.Model):
-    title = models.TextField()
+class TestModel(models.Model):
+  title = models.TextField()
 
-  def setUp(self):
-    self.test_model = UtilsTestCase.TestModel()
 
-  def test_copy_django_model_attrs_copies_correct_attribute(self):
-    title = 'hi'
-    attrs = {'title': title}
-    copy_django_model_attrs(self.test_model, **attrs)
-    self.assertEqual(title, self.test_model.title)
+@pytest.fixture
+def test_model():
+  test_model = TestModel()
+  return test_model
 
-  def test_copy_django_model_attrs_copies_correct_attribute_only_if_called(self):
-    self.assertFalse(self.test_model.title)
 
-  def test_copy_django_model_attrs_does_not_copy_pk(self):
-    id_field = 1
-    attrs = {'pk': id_field, 'id': id_field}
-    copy_django_model_attrs(self.test_model, **attrs)
-    self.assertNotEqual(id_field, self.test_model.pk)
+def test_copy_django_model_attrs_copies_correct_attribute(test_model):
+  title = 'hi'
+  attrs = {'title': title}
+  copy_django_model_attrs(test_model, **attrs)
+  assert title == test_model.title
+
+
+def test_copy_django_model_attrs_copies_correct_attribute_only_if_called(test_model):
+  assert bool(test_model.title) == False
+
+
+def test_copy_django_model_attrs_does_not_copy_pk(test_model):
+  id_field = 1
+  attrs = {'pk': id_field, 'id': id_field}
+  copy_django_model_attrs(test_model, **attrs)
+  assert id_field != test_model.pk
 
