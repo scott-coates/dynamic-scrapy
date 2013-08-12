cccf89ecf84d008a9e248f0577f0a4f93336f210
Scott Coates <scoarescoare@gmail.com>
2013-08-12 09:25:57 -0400

    phone test

--- scrapy_test/libs/communication_utils/tests/unit/contact_parser_tests.py
+++ scrapy_test/libs/communication_utils/tests/unit/contact_parser_tests.py
@@ -4,6 +4,7 @@ from scrapy_test.libs.communication_utils.parsing import contact_parser
 
 @pytest.mark.parametrize(("input_values", "expected"), [
   ('abc 555-555-5555 abc 123', '(555) 555-5555'),
+  ('Caliber Associates, Inc. (646) 597-6005', '(646) 597-6005'),
 ])
 def test_contact_parser_detects_phone_number(input_values, expected):
   assert expected == contact_parser.get_contact_phone_number(input_values)
