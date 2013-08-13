51da8ce8333924dd09d9a7e183902b366626b6a3
Scott Coates <scoarescoare@gmail.com>
2013-08-13 13:35:44 -0400

    return CanonicalNameResult

--- scrapy_test/libs/text_utils/parsers/text_parser.py
+++ scrapy_test/libs/text_utils/parsers/text_parser.py
@@ -1,7 +1,10 @@
 import logging
 from scrapy_test.libs.text_utils.formatting.text_formatter import only_alpha_numeric
+from collections import namedtuple
 
 logger = logging.getLogger(__name__)
+
+CanonicalNameResult = namedtuple('CanonicalNameResult', 'keyword_id is_truthy')
 
 
 def get_canonical_name_from_keywords(content, keywords):
@@ -13,8 +16,8 @@ def get_canonical_name_from_keywords(content, keywords):
   for k, v in keywords.items():
     if " " in k:
       if only_alpha_numeric(k).lower() in content_alnum:
-        ret_val.append(v)
+        ret_val.append(CanonicalNameResult(v, True))
     elif only_alpha_numeric(k).lower() in content_words:
-      ret_val.append(v)
+      ret_val.append(CanonicalNameResult(v, True))
 
   return list(set(ret_val))
--- scrapy_test/libs/text_utils/tests/unit/test_text_parser.py
+++ scrapy_test/libs/text_utils/tests/unit/test_text_parser.py
@@ -1,11 +1,12 @@
 import pytest
 from scrapy_test.libs.text_utils.parsers import text_parser
+from scrapy_test.libs.text_utils.parsers.text_parser import CanonicalNameResult
 
 
 @pytest.mark.parametrize(("input_values", "keywords", "expected"), [
-  ('hi how are you', {'hi':1}, [1]),
+  ('hi how are you', {'hi':1}, [CanonicalNameResult(1,True)]),
   ('how are you', {'hi':1}, []),
-  ('this foo bar is so dumb', {'foo bar':1}, [1]),
+  ('this foo bar is so dumb', {'foo bar':1}, [CanonicalNameResult(1,True)]),
 ])
 def test_text_parser_detects_correct_keywords(input_values, keywords, expected):
   assert expected == text_parser.get_canonical_name_from_keywords(input_values,keywords)
