e50d7bd105b1405bbf547cbed097696287330e67
Scott Coates <scoarescoare@gmail.com>
2013-08-23 16:20:07 -0400

    added listing geo service

--- scrapy_test/libs/geo_utils/services/geo_location_service.py
+++ scrapy_test/libs/geo_utils/services/geo_location_service.py
@@ -1,7 +1,8 @@
 from scrapy_test.libs.geo_utils.sanitized_address import SanitizedAddress
-
+from pygeocoder import Geocoder
 
 def get_sanitized_address(lat, lng, address1, address2, city, state, zip_code):
-  listing = {}
+  if lat and lng:
+    results = Geocoder.reverse_geocode(lat,lng)
   return SanitizedAddress(listing.lat, listing.lng, listing.address1, listing.address2.listing.city, listing.state,
                           listing.zip_code)
