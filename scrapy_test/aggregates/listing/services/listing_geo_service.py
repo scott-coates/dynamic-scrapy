ecfa3c1d4c550a843502b44d87e557b4f7d0d8b7
Scott Coates <scoarescoare@gmail.com>
2013-08-24 13:20:08 -0400

    cross street test

--- scrapy_test/aggregates/listing/services/listing_geo_service.py
+++ scrapy_test/aggregates/listing/services/listing_geo_service.py
@@ -1,5 +1,5 @@
 from scrapy_test.aggregates.listing.models import Listing
-from scrapy_test.libs.geo_utils.geocoded_address import GeocodedAddress
+from scrapy_test.aggregates.listing.value_objects import SanitizedAddress
 from scrapy_test.libs.geo_utils.services import geo_location_service
 
 
@@ -7,7 +7,7 @@ def get_sanitized_address(address, city, state,
                           _listing_manager=Listing.objects, _geo_location_service=geo_location_service):
   try:
     existing_listing = _listing_manager.find_from_address(address, city, state)
-    sanitized_listing = GeocodedAddress(existing_listing.lat,
+    sanitized_listing = SanitizedAddress(existing_listing.lat,
                                         existing_listing.lng,
                                         existing_listing.address,
                                         existing_listing.city,
@@ -17,10 +17,18 @@ def get_sanitized_address(address, city, state,
   except Listing.DoesNotExist:
     geocoded_listing = _geo_location_service.get_geocoded_address(address, city, state)._asdict()
 
+    formatted_parts = geocoded_listing['formatted_address'].split(',')
+
+    address1 = geocoded_listing.get('address1')
+    if address1:
+      geocoded_listing.pop('address1')
+      geocoded_listing['address'] = formatted_parts[0]
+
     address2 = geocoded_listing.get('address2')
     if address2:
+      geocoded_listing.pop('address2')
       address2_formatted = ' #{0}'.format(address2)
       geocoded_listing['formatted_address'] = geocoded_listing['formatted_address'].replace(address2_formatted, '')
 
-    sanitized_listing = GeocodedAddress(**geocoded_listing)
+    sanitized_listing = SanitizedAddress(**geocoded_listing)
   return sanitized_listing
