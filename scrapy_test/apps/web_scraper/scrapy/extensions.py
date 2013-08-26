185037305d74489a6eee85b667a4bd619033adaa
Scott Coates <scoarescoare@gmail.com>
2013-08-26 16:33:32 -0400

    #get first item in last_updated_date or None

--- scrapy_test/apps/web_scraper/scrapy/extensions.py
+++ scrapy_test/apps/web_scraper/scrapy/extensions.py
@@ -24,7 +24,8 @@ class StopOnDuplicateItem(object):
         # don't stop crawling. This could happen if
         # we first encounter it on page 100.html but new posts appeared and forced this to 200.html, the next page.
 
-        if len(item['last_updated_date']) and item['last_updated_date'][0] == duplicate.last_updated_date:
+        #get first item in last_updated_date or None
+        if next((x for x in item['last_updated_date']), None) == duplicate.last_updated_date:
         # if this listing was created a long time ago but just recently re-newed or re-updated.
 
           if timezone.now() - duplicate.changed >= timedelta(hours=1):
