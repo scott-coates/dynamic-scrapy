07c9474468ba88639a189a860a90970a169cd3be
Scott Coates <scoarescoare@gmail.com>
2013-08-27 14:47:20 -0400

    added event handlers and signals

---
+++ scrapy_test/aggregates/apartment/event_handlers.py
@@ -0,0 +1,9 @@
+from django.dispatch import receiver
+from scrapy_test.aggregates.apartment.services import apartment_tasks
+from scrapy_test.aggregates.listing.signals import sanitized
+from scrapy_test.aggregates.listing.models import Listing
+
+
+@receiver(sanitized,sender = Listing)
+def event_occurred_callback(sender, **kwargs):
+  apartment_tasks.associate_listing_with_apartment_task.delay(kwargs['instance'].id)
