306adbfa9e20b4e5606e2537e785c362f88a4ac9
Scott Coates <scoarescoare@gmail.com>
2013-08-28 11:38:28 -0400

    only follow aggregate

--- scrapy_test/aggregates/apartment/reversions.py
+++ scrapy_test/aggregates/apartment/reversions.py
@@ -1,4 +1,4 @@
 import reversion
 from scrapy_test.aggregates.apartment.models import Apartment
 
-reversion.register(Apartment, follow=['listings'])
+reversion.register(Apartment)
--- scrapy_test/aggregates/listing/reversions.py
+++ scrapy_test/aggregates/listing/reversions.py
@@ -2,4 +2,4 @@ import reversion
 from scrapy_test.aggregates.listing.models import Listing, Amenity
 
 reversion.register(Listing, follow=['amenities'])
-reversion.register(Amenity, follow=['amenity_type'])
+reversion.register(Amenity)
