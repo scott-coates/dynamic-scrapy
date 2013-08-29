2b9761ba40ba98cd03bdcf106866cbbb0a9ad4f2
Scott Coates <scoarescoare@gmail.com>
2013-08-29 13:34:53 -0400

    added cl_listing_4033538277

--- scrapy_test/aggregates/listing/tests/listing_test_data.py
+++ scrapy_test/aggregates/listing/tests/listing_test_data.py
@@ -19,3 +19,26 @@ cl_listing_3952467416_expected_description = u'HOUSE FOR RENT IN GOOD LOCATION\n
 cl_listing_3952467416_expected_posted_date = eastern_time_zone.localize(datetime.datetime(2013, 8, 7, 16, 52))
 cl_listing_3952467416_expected_last_updated_date = eastern_time_zone.localize(datetime.datetime(2013, 7, 22, 15, 31))
 #endregion
+
+# region cl_listing_4033538277
+cl_listing_4033538277 = {u'city': [u'brooklyn'], u'contact_phone_number': [u'bedstuy / clinton hill'],
+                         u'description': [u'\n\t\tBeautiful 3 Bedroom 2 Full bath', u'\nAmazing Finishes',
+                                          u'\nHuge Backyard',
+                                          u'\n100% no fee By owner',
+                                          u'\nAll Bedrooms can fit King and Queen sized beds',
+                                          u'\nSteps to the G train', u'\nLaundry in the Building',
+                                          u'\nClose to All your needs',
+                                          u'\nNo brokers Please', u'\nCall or Text Danny @ 646 338 3852 ',
+                                          u'\n3526+56+5\n\t'],
+                         u'title': [
+                           u'\n  - $2695 / 3br - 3 Bedroom 2 Full bath + Massive Backyard~Prime Location (bedstuy / '
+                           u'clinton hill)\n'],
+                         u'url': u'http://newyork.craigslist.org/brk/abo/4033538277.html', u'state': [u'ny'],
+                         u'last_updated_date': [],
+                         u'posted_date': [u'2013-08-29, 12:55PM EDT', u'2013-08-29, 12:55PM EDT'],
+                         u'contact_email_address': [u'bedstuy / clinton hill'],
+                         u'address': [u'vernon ave at nostrand', u'vernon ave', u'nostrand'], u'lat': [u'40.694263'],
+                         'listing_source_id': 1,
+                         u'lng': [u'-73.952341'], u'broker_fee': [u'/brk/abo/4033538277.html'],
+                         u'contact_name': [u'bedstuy / clinton hill']}
+#endregion
