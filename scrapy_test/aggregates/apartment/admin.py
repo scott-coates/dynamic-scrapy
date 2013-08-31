31960eea26a3afc876874efd27e8171b57925bab
Scott Coates <scoarescoare@gmail.com>
2013-08-31 13:02:09 -0400

    don't add more

--- scrapy_test/aggregates/apartment/admin.py
+++ scrapy_test/aggregates/apartment/admin.py
@@ -1,16 +1,23 @@
 from django.contrib import admin
-from scrapy_test.aggregates.apartment.models import Apartment
+from scrapy_test.aggregates.apartment.models import Apartment, Amenity
 from scrapy_test.aggregates.listing.models import Listing
 
 
 class ListingInline(admin.TabularInline):
   model = Listing
+  max_num = 0
 
 
-class AuthorAdmin(admin.ModelAdmin):
+class AmenityInline(admin.StackedInline):
+  model = Amenity
+  max_num = 0
+
+
+class ApartmentAdmin(admin.ModelAdmin):
   inlines = [
     ListingInline,
+    AmenityInline,
   ]
 
 
-admin.site.register(Apartment, AuthorAdmin)
+admin.site.register(Apartment, ApartmentAdmin)
