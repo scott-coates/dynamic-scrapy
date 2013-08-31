e84ee3ffc2ffab81cb868442d205e58d2f2b7aa0
Scott Coates <scoarescoare@gmail.com>
2013-08-31 12:05:33 -0400

    studio test

--- scrapy_test/libs/housing_utils/tests/unit/test_home_parser.py
+++ scrapy_test/libs/housing_utils/tests/unit/test_home_parser.py
@@ -6,6 +6,7 @@ from scrapy_test.libs.housing_utils.parsing import home_parser
   ('studio', 0),
   ('1br', 1),
   ('$5300 / 3br - 1500ft - W-O-N-D-E-R-F-U-L APT____Doorman, Roofdeck/GYM - NO FEE!!! (East Village', 3),
+  ('$2750 / 400ft - Bright studio with great closet space and city view (Midtown West)', 0),
 ])
 def test_home_parser_detects_correct_bedroom_count(input_values, expected):
   assert expected == home_parser.get_bedroom_count(input_values)
