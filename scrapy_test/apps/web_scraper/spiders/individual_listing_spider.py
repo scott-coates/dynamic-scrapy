1bb0ba517dd18d5fbc91e76d3fee0f3e8644ce45
Scott Coates <scoarescoare@gmail.com>
2013-09-07 16:24:56 -0400

    added fuzy search for finding listig source

--- scrapy_test/apps/web_scraper/spiders/individual_listing_spider.py
+++ scrapy_test/apps/web_scraper/spiders/individual_listing_spider.py
@@ -2,6 +2,8 @@ from urlparse import urlparse
 from scrapy_test.aggregates.listing_source.models import ListingSource
 from scrapy_test.apps.web_scraper.spiders.individual_item_spider import IndividualItemSpider
 from scrapy_test.apps.web_scraper.spiders.listing_spider import ListingSpider
+from scrapy_test.libs.text_utils.search import fuzzy_search
+
 
 class IndividualListingSpider(IndividualItemSpider, ListingSpider):
   name = 'individual_listing_spider'
@@ -14,9 +16,18 @@ class IndividualListingSpider(IndividualItemSpider, ListingSpider):
 
     #get the root domain - but this is pretty naive. consider
     #https://github.com/john-kurkowski/tldextract
-    domain = urlparse(url).netloc.split(".")[1]
+    netloc_split = urlparse(url).netloc.split(".")
+    if 'www' in netloc_split:
+      domain = netloc_split[1]
+    else:
+      domain = netloc_split[0]
 
-    config = ListingSource.objects.filter(url__icontains=domain)[:1].get().scraper_config
+    listing_sources = ListingSource.objects.filter(url__icontains=domain).all()
+    source_dict = {k.url: k for k in listing_sources}
+
+    closest_url = fuzzy_search.get_closest_word(url, source_dict.keys())
+
+    config = source_dict[closest_url].scraper_config
 
     self.scraper = config.scraper
     self.scrape_url = url
