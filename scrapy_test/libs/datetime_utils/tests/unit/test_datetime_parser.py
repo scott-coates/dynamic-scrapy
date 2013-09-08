78f6aee9e5184cd702ed4d0ba84f8b79270cecb5
Scott Coates <scoarescoare@gmail.com>
2013-09-07 22:14:37 -0400

    added datetime parsing for relative keywords

--- scrapy_test/libs/datetime_utils/tests/unit/test_datetime_parser.py
+++ scrapy_test/libs/datetime_utils/tests/unit/test_datetime_parser.py
@@ -1,5 +1,10 @@
 import datetime
+
+from django.utils import timezone
+from mock import MagicMock
 import pytest
+from django.utils.timezone import utc
+
 from scrapy_test.aggregates.listing.tests.listing_test_data import eastern_time_zone
 from scrapy_test.libs.datetime_utils.parsers import datetime_parser
 
@@ -9,3 +14,12 @@ from scrapy_test.libs.datetime_utils.parsers import datetime_parser
 ])
 def test_datetime_parser_detects_correct_timezone(input_values, expected):
   assert expected == datetime_parser.get_datetime(input_values)
+
+
+def test_datetime_parser_uses_relative_keywords():
+  timezone_mock = MagicMock(spec=timezone)
+  timezone_mock.now = MagicMock(return_value=datetime.datetime(2042, 1, 5, tzinfo=utc))
+
+  expected = datetime.datetime(2042, 1, 1, tzinfo=utc)
+  actual = datetime_parser.get_datetime('4 days on market', _datetime_service=timezone_mock)
+  assert expected == actual
