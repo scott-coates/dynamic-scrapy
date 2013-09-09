f893576676319a53ac9b652f1935a0a112262d51
Scott Coates <scoarescoare@gmail.com>
2013-09-09 00:15:37 -0400

    added trusted geo column

--- scrapy_test/aggregates/listing_source/models.py
+++ scrapy_test/aggregates/listing_source/models.py
@@ -7,6 +7,7 @@ logger = logging.getLogger(__name__)
 class ListingSource(models.Model):
   name = models.CharField(max_length=200)
   url = models.URLField()
+  trusted_geo_data = models.BooleanField()
 
   def __unicode__(self):
     return self.name
