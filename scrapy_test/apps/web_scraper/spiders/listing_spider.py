8369f7d985ed661c6e61d571303833411a98ec84
Scott Coates <scoarescoare@gmail.com>
2013-09-09 18:22:13 -0400

    replace to original url

--- scrapy_test/apps/web_scraper/spiders/listing_spider.py
+++ scrapy_test/apps/web_scraper/spiders/listing_spider.py
@@ -46,6 +46,7 @@ class ListingSpider(DjangoSpider):
     if pre_defined_scraper:
       self.scraper = temp_scraper
       ret_val = parsed_item
+      response.url = self.scrape_url
     else:
       # some sites like streeteasy have the lstings spread over manyy differnt pages and DDS will not natively work
       # that way. To get around it, we'll not return the parsed item just yet. We'll hand off a request to parse the
