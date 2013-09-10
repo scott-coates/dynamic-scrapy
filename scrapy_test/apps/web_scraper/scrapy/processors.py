e70567bea30c615c29a119a99a51ccd87091a432
Scott Coates <scoarescoare@gmail.com>
2013-09-09 20:51:25 -0400

    added replace proc

--- scrapy_test/apps/web_scraper/scrapy/processors.py
+++ scrapy_test/apps/web_scraper/scrapy/processors.py
@@ -1,10 +1,20 @@
 from urlparse import urljoin
 
 
-#this is called from the admin. The listing spider defins this method as a processor and is invoked dynamically
+#this is called from the admin. The listing spider defines this method as a processor and is invoked dynamically
 def pre_url_from_ref_object(text, loader_context):
   base_url = loader_context['spider'].ref_object.listing_source.url
 
   ret_val = urljoin(base_url, text[0])
+
+  return ret_val
+
+def replace(text, loader_context):
+  ret_val = [ ]
+
+  for x in text:
+    for k,v in loader_context['replace'].items():
+      x = x.replace(k,v)
+    ret_val.append(x)
 
   return ret_val
