83cdb5aac2398d76f5e50645634d036457cb5119
Scott Coates <scoarescoare@gmail.com>
2013-09-10 07:58:26 -0400

    fixed test data

--- scrapy_test/aggregates/listing/tests/listing_test_data.py
+++ scrapy_test/aggregates/listing/tests/listing_test_data.py
@@ -14,8 +14,9 @@ cl_listing_3952467416_posted_date = [u'2013-08-07,  4:52PM EDT', u'2013-08-07,  
 cl_listing_3952467416_last_updated_date = [u'2013-07-22,  3:31PM EDT']
 
 cl_listing_3952467416_expected_title = '$1750 / 3br - BEST AREA IN FAR ROCKAWAY (FAR ROCKAWAY)'
-cl_listing_3952467416_expected_description = u'HOUSE FOR RENT IN GOOD LOCATION\n3 BED ROOM LIVING ROOM DINING ' \
-                                             u'ROOM\nNEW KITCHEN \nCALL DIANA 718-343-2739\nNO BROKERS FEE'
+cl_listing_3952467416_expected_description = u'HOUSE FOR RENT IN GOOD LOCATION\n\n3 BED ROOM LIVING ROOM DINING ' \
+                                             u'ROOM\n\nNEW KITCHEN\n\nCALL DIANA 718-343-2739\n\nNO BROKERS FEE'
+
 cl_listing_3952467416_expected_posted_date = eastern_time_zone.localize(datetime.datetime(2013, 8, 7, 16, 52))
 cl_listing_3952467416_expected_last_updated_date = eastern_time_zone.localize(datetime.datetime(2013, 7, 22, 15, 31))
 #endregion
