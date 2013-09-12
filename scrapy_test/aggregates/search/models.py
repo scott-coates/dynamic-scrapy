f6a22d078f4b429bdb05b1eece626ebd081dc686
Scott Coates <scoarescoare@gmail.com>
2013-09-12 00:21:04 -0400

    added more about search

--- scrapy_test/aggregates/search/models.py
+++ scrapy_test/aggregates/search/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from localflavor.us.models import USStateField
+
 from scrapy_test.libs.common_domain.aggregate_base import AggregateBase
 
 
@@ -6,8 +8,14 @@ class Search(models.Model, AggregateBase):
   description = models.TextField()
 
   specified_location = models.CharField(max_length=2048)
+
+  address = models.CharField(max_length=255, blank=True, null=True)
+  city = models.CharField(max_length=255)
+  state = USStateField()
+  zip_code = models.CharField(max_length=10, blank=True, null=True)
   lat = models.FloatField()
   lng = models.FloatField()
+  formatted_address = models.CharField(max_length=4096)
 
   no_fee_preferred = models.BooleanField()
 
@@ -19,3 +27,12 @@ class Search(models.Model, AggregateBase):
   price_max = models.DecimalField(max_digits=10, decimal_places=2)
   sqfeet_min = models.DecimalField(max_digits=10, decimal_places=3, blank=True, null=True)
   sqfeet_max = models.DecimalField(max_digits=10, decimal_places=3, blank=True, null=True)
+
+
+class Amenity(models.Model):
+  is_available = models.BooleanField()
+  apartment = models.ForeignKey(Search, related_name='amenities')
+  amenity_type = models.ForeignKey('amenity.Amenity', related_name='search_instance')
+
+  class Meta:
+    unique_together = ("search", "amenity_type")
