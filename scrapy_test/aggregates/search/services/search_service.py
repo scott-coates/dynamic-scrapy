440fa43fe688a82fbd975ec1bc2028af94564c01
Scott Coates <scoarescoare@gmail.com>
2013-09-12 14:00:13 -0400

    search service gets geo

--- scrapy_test/aggregates/search/services/search_service.py
+++ scrapy_test/aggregates/search/services/search_service.py
@@ -3,7 +3,21 @@ from scrapy_test.libs.geo_utils.services import geo_location_service
 
 
 def create_search(_geo_location_service=geo_location_service, **search_attrs):
-  search = factories.construct_apartment_from_listing(**search_attrs)
+  specified_location = search_attrs.get('specified_location', None)
+
+  if not specified_location: raise TypeError('specified location is required')
+
+  geocoded_address = _geo_location_service.get_geocoded_address(specified_location)
+
+  search_attrs['address'] = geocoded_address.address1
+  search_attrs['city'] = geocoded_address.city
+  search_attrs['state'] = geocoded_address.state
+  search_attrs['zip_code'] = geocoded_address.zip_code
+  search_attrs['formatted_address'] = geocoded_address.formatted_address
+  search_attrs['lat'] = geocoded_address.lat
+  search_attrs['lng'] = geocoded_address.lng
+
+  search = factories.construct_search(**search_attrs)
 
   return search
 
