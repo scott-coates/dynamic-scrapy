b81db3726c20b9244c13ce6197334120cac4e8d1
Scott Coates <scoarescoare@gmail.com>
2013-09-12 16:25:04 -0400

    added unit tests for search creation

---
+++ scrapy_test/aggregates/search/tests/unit/test_search_aggregate.py
@@ -0,0 +1,23 @@
+from django.core.exceptions import ValidationError
+import pytest
+from scrapy_test.aggregates.search.models import Search
+
+
+@pytest.mark.parametrize(("min_value", "max_value", "search_attr"), [
+  (3, 2, 'bedroom'),
+  (3.5, 2.5, 'bathroom'),
+  (1000, 850.50, 'sqfeet'),
+  (3500, 2500.00, 'price'),
+])
+def test_search_throws_appropriate_error_for_invalid_max_settings(min_value, max_value, search_attr):
+  search_attrs = {
+    'description': 'My Search',
+    'specified_location': 'My Location',
+    'geo_boundary_points': [{}, {}, {}],
+    search_attr + "_min": min_value,
+    search_attr + "_max": max_value,
+
+  }
+
+  with pytest.raises(ValidationError):
+    Search._from_attrs(**search_attrs)
