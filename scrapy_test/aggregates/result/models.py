82ec2b1110f19846a04c4b19a02a7f0944352211
Scott Coates <scoarescoare@gmail.com>
2013-09-12 16:50:46 -0400

    added result props

--- scrapy_test/aggregates/result/models.py
+++ scrapy_test/aggregates/result/models.py
@@ -8,6 +8,14 @@ logger = logging.getLogger(__name__)
 
 
 class Result(models.Model, AggregateBase):
+  apartment = models.ForeignKey('apartment.Apartment', related_name="results")
+
+  search = models.ForeignKey('search.Search', related_name="results")
+
+  compliance = models.PositiveSmallIntegerField(max_length=2)
+
+  created = models.DateTimeField(auto_now_add=True)
+  changed = models.DateTimeField(auto_now=True)
 
   def __unicode__(self):
     return 'Result #' + str(self.pk)
