209552e8ef6bbc80a9462f030096f7e81fb48634
Scott Coates <scoarescoare@gmail.com>
2013-09-12 22:28:39 -0400

    100 %

--- scrapy_test/aggregates/result/tests/unit/test_calc_compliance.py
+++ scrapy_test/aggregates/result/tests/unit/test_calc_compliance.py
@@ -8,6 +8,11 @@ from scrapy_test.aggregates.search.models import Search
   (Apartment(), Search(), 0),
   (Apartment(), Search(bedroom_min=2), 0),
   (Apartment(bedroom_count=2), Search(bedroom_min=2), 33),
+  (
+    Apartment(bedroom_count=2, bathroom_count=2, price=1000),
+    Search(bedroom_min=1, bathroom_min=1, price_max=1200),
+    100
+  ),
 ])
 def test_result_calcs_compliance_score_correctly(apartment, search, expected_compliance_score):
   result = Result._from_apartment_and_search(apartment, search)
