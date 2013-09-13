2b591e5393a74a311eddb02e2ab348a939ac0749
Scott Coates <scoarescoare@gmail.com>
2013-09-13 16:52:10 -0400

    created compliance score integration test

---
+++ scrapy_test/aggregates/result/tests/integration/test_calc_compliance.py
@@ -0,0 +1,27 @@
+import pytest
+from scrapy_test.aggregates.apartment.models import Amenity as ApartmentAmenity
+from scrapy_test.aggregates.listing.models import Listing
+from scrapy_test.aggregates.listing.services import listing_service
+from scrapy_test.aggregates.result.services import result_service
+from scrapy_test.aggregates.result.tests import result_test_data
+from scrapy_test.aggregates.search.services import search_service
+
+
+@pytest.mark.django_db_with_migrations
+def test_result_creates_correct_compliance_score():
+
+  listing = listing_service.create_listing(**result_test_data.cl_listing_4033538277)
+  listing = Listing.objects.get(pk=listing.id)
+
+  apartment = listing.apartment
+
+  apartment.amenities.add(ApartmentAmenity(amenity_type_id=1, is_available=True),
+                          ApartmentAmenity(amenity_type_id=2, is_available=True),
+                          ApartmentAmenity(amenity_type_id=3, is_available=False))
+
+  search = search_service.create_search(**result_test_data.search_1)
+
+  result = result_service.create_result(apartment, search)
+
+
+  assert result.compliance_score == 60
--- scrapy_test/aggregates/result/tests/unit/test_calc_compliance.py
+++ scrapy_test/aggregates/result/tests/unit/test_calc_compliance.py
@@ -1,20 +1,7 @@
-from mock import MagicMock
 import pytest
 from scrapy_test.aggregates.apartment.models import Apartment
 from scrapy_test.aggregates.result.models import Result
 from scrapy_test.aggregates.search.models import Search
-
-
-apartment_with_amenity_1_mock = MagicMock(wraps=Apartment(), bedroom_count=2, bathroom_count=2, price=1000, sqfeet=500)
-apartment_with_amenity_1_mock.amenities = MagicMock(
-  values_list=MagicMock(return_value=[(1, True), (2, True), (3, False)]),
-)
-search_with_amenity_1_mock = MagicMock(wraps=Search(), bedroom_min=1, bathroom_min=1, price_max=1200, sqfeet_min=100)
-
-search_with_amenity_1_mock.amenities = MagicMock(
-  count=MagicMock(return_value=3),
-  all=MagicMock(return_value=MagicMock(values_list=MagicMock(return_value=[1, 2, 3])),
-  ))
 
 
 @pytest.mark.parametrize(("apartment", "search", "expected_compliance_score"), [
@@ -25,11 +12,6 @@ search_with_amenity_1_mock.amenities = MagicMock(
     Apartment(bedroom_count=2, bathroom_count=2, price=1000),
     Search(bedroom_min=1, bathroom_min=1, price_max=1200),
     100
-  ),
-  (
-    apartment_with_amenity_1_mock,
-    search_with_amenity_1_mock,
-    86
   ),
 ])
 def test_result_calcs_compliance_score_correctly(apartment, search, expected_compliance_score):
