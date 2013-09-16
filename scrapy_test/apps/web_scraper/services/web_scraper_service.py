bd255a629d0714f8161993cf8b2ee3b70f96bb68
Scott Coates <scoarescoare@gmail.com>
2013-09-16 17:44:38 -0400

    a day wait

--- scrapy_test/apps/web_scraper/services/web_scraper_service.py
+++ scrapy_test/apps/web_scraper/services/web_scraper_service.py
@@ -1,6 +1,7 @@
+from datetime import timedelta
+from django.utils import timezone
 from dynamic_scraper.models import SchedulerRuntime
 from dynamic_scraper.utils.task_utils import ProcessBasedUtils
-from scrapy_test.aggregates.listing.services import listing_service
 from scrapy_test.apps.web_scraper.models import ListingSourceScraperConfig, ListingCheckerConfig
 from scrapy_test.apps.web_scraper.scrapy.task_utils import IndividualProcessBasedItemLauncher
 
@@ -9,9 +10,11 @@ def run_spiders():
   t = ProcessBasedUtils()
   t.run_spiders(ListingSourceScraperConfig, 'scraper', 'scraper_runtime', 'listing_spider')
 
+
 def run_checkers():
   t = ProcessBasedUtils()
   t.run_checkers(ListingCheckerConfig, 'scraper', 'checker_runtime', 'listing_checker')
+
 
 def run_individual_listing_spider(url):
   t = IndividualProcessBasedItemLauncher()
@@ -21,7 +24,7 @@ def run_individual_listing_spider(url):
 def add_listing_checker(listing):
   listing_source_cfg = ListingSourceScraperConfig.objects.get(pk=listing.listing_source_id)
 
-  checker_rt = SchedulerRuntime(runtime_type='C')
+  checker_rt = SchedulerRuntime(runtime_type='C', next_action_time=timezone.now() + timedelta(days=1))
   checker_rt.save()
 
   checker_config = ListingCheckerConfig(listing=listing, checker_runtime=checker_rt, scraper=listing_source_cfg.scraper)
