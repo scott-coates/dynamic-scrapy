2e0dbe63a3114188b104a77248b5bb9ca2b219e1
Scott Coates <scoarescoare@gmail.com>
2013-09-16 23:57:19 -0400

    added comm app

--- scrapy_test/settings/common.py
+++ scrapy_test/settings/common.py
@@ -218,6 +218,7 @@ LOCAL_APPS = (
   'scrapy_test.aggregates.result',
   'scrapy_test.aggregates.search',
   # APPS
+  'scrapy_test.apps.communication',
   'scrapy_test.apps.web_scraper',
   # LIBS
   'scrapy_test.libs.common_domain',
