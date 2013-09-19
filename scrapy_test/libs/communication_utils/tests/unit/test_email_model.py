8b0a0a5f926a7b2bd6782a43e22c0b7e24ea7d13
Scott Coates <scoarescoare@gmail.com>
2013-09-18 21:10:07 -0400

    test from

--- scrapy_test/libs/communication_utils/tests/unit/test_email_model.py
+++ scrapy_test/libs/communication_utils/tests/unit/test_email_model.py
@@ -15,12 +15,6 @@ def test_email_service_detects_spam(input_values, expected):
   assert expected == email_service.is_spam(**input_values)
 
 
-def test_email_service_sets_direction():
+def test_email_service_constructs_email1():
   email = email_service.construct_email(**email_1)
   assert email.email_direction == Email.email_direction_incoming
-
-
-def test_email_service_corrects_from_keyword():
-  from_address = 'something@test.com'
-  email = email_service.construct_email(**dict(email_1, **{'from': from_address}))
-  assert email.from_address == from_address
