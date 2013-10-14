49cf30a834cb6e80f4ec4c5aeea55e2112bddb3e
Scott Coates <scoarescoare@gmail.com>
2013-10-14 18:18:40 -0400

    fix bug: # for some reason, model_to_dict converts the value to a string

--- scrapy_test/apps/domain/search/services/potential_search_service.py
+++ scrapy_test/apps/domain/search/services/potential_search_service.py
@@ -1,3 +1,4 @@
+import json
 from django.forms import model_to_dict
 from scrapy_test.aggregates.search.models import Search
 from scrapy_test.apps.domain.search.models import PotentialSearch
@@ -12,4 +13,12 @@ def get_potential_search(pk):
 
 
 def get_search_attrs(search_attrs_dict):
-  return model_to_dict(Search(**search_attrs_dict), fields=search_attrs_dict.keys())
+  search = Search(**search_attrs_dict)
+
+  search_dict = model_to_dict(search, fields=search_attrs_dict.keys())
+
+  # for some reason, model_to_dict converts the value to a string
+  if 'geo_boundary_points' in search_dict:
+    search_dict['geo_boundary_points']  = json.loads(search_dict['geo_boundary_points'])
+
+  return search_dict
