f665878c3667531667bb69388bf5b0c495a5ff6b
Scott Coates <scoarescoare@gmail.com>
2013-11-17 22:14:16 -0500

    log email

--- scrapy_test/libs/communication_utils/services/emailer.py
+++ scrapy_test/libs/communication_utils/services/emailer.py
@@ -1,11 +1,17 @@
+import logging
 import sendgrid
 from django.conf import settings
 
 emailer = sendgrid.Sendgrid(settings.SENDGRID_USERNAME, settings.SENDGRID_PASSWORD, secure=True)
+
+logger = logging.getLogger(__name__)
 
 
 def send_email(from_address, from_name, to_address, subject, text, html):
   msg = sendgrid.Message((from_address, from_name), subject, text, html)
   msg.add_to(to_address)
 
-  emailer.smtp.send(msg)
+  if settings.DEBUG:
+    emailer.smtp.send(msg)
+  else:
+    logger.info(msg)
